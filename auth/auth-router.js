1b241364d4274309c1bb568deae71f466b6d8655
John Rojcewicz <rojcewiczj@gmail.com>
2019-10-18 10:36:14 -0500

    completed auth router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,13 +2,59 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const Users = require('../users/users-model.js');
+const secrets = require('../config/secrets.js');
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'cannot add the user', error });
+    });
 });
 
 router.post('/login', (req, res) => {
   // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        // produce token
+        const token = generateToken(user);
+
+        // add token to response
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+    password: user.password,
+    subject: user.id,
+  };
+  const options = {
+    expiresIn: '1h',
+  };
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
 
 module.exports = router;
