4ecc4a9bee7867ddae572e43587826005cbec248
John Rojcewicz <rojcewiczj@gmail.com>
2019-10-18 10:39:24 -0500

    included middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,29 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require('jsonwebtoken');
+
+const secrets = require('../config/secrets.js');
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  if (token) {
+   
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if (err) {
+        
+        res.status(401).json({ message: 'you must answer three riddles!' });
+      } else {
+      
+        req.user = {
+          username: decodedToken.username,
+          role: decodedToken.role,
+        };
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: 'No token provided' });
+  }
 };
