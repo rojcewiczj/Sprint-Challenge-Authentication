66a33da029ecd2ec0c736167fea3d3dad1065387
John Rojcewicz <rojcewiczj@gmail.com>
2019-10-18 10:31:14 -0500

    created a user-model

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 
 router.post('/register', (req, res) => {
   // implement registration
